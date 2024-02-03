22ba81b1dd92b276964cb852affba233b9d6c8c9
Sravanthi <100075866+mrgsravanthi@users.noreply.github.com>
2024-02-03 17:45:12 +0530

    Update Evaluation.py

--- Evaluation.py
+++ Evaluation.py
@@ -2,29 +2,24 @@ from sklearn.metrics import classification_report, confusion_matrix, precision_r
 import numpy as np
 from scipy.special import softmax
 import xgboost as xgb
-
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 def evaluate(model, X_test, y_test):
-    logits = model.predict(X_test)
-    probs = softmax(logits, axis=1)
-    classes = np.argmax(probs, axis=1)
-    print_metrics(classes, y_test, probs[:,1])
+    print("X_test shape:", X_test.shape)
+    predictions = model.predict(X_test)
+    print_regression_metrics(predictions, y_test)
 
 
 def evaluate_XGB(obj, X_test, y_test):
     dtest = xgb.DMatrix(data=X_test)
-    probs = obj.predict(dtest)
-    classes = probs.copy()
-    classes[classes > 0.5] = 1
-    classes[classes <= 0.5] = 0
-    print_metrics(classes, y_test, probs)
+    predictions = obj.predict(dtest)
+    print_regression_metrics(predictions, y_test)
 
 
-def print_metrics(classes, y_test, probs):
-    print(classification_report(classes, y_test, labels=[0, 1]))
-    prec, recall, thr = precision_recall_curve(y_test, probs, pos_label=1)
-    prauc = auc(recall, prec)
-    print(prauc)
-    prauc = roc_auc_score(y_test, probs)
-    print(prauc)
-    print(confusion_matrix(classes, y_test))
+def print_regression_metrics(predictions, y_test):
+    
+    mse = mean_squared_error(y_test, predictions)
+    mae = mean_absolute_error(y_test, predictions)
+
+    print(f"Mean Squared Error: {mse}")
+    print(f"Mean Absolute Error: {mae}")
